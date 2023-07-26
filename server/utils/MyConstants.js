b6563b02931aa74da0f54f8db7f13c8ce124af3a
Huy Vu <116423081+VoHuyVu@users.noreply.github.com>
2023-07-26 22:26:51 +0700

    Add files via upload
    1

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'shopppingbook.wijqear.mongodb.net',
+    DB_USER: 'nghiapham1144',
+    DB_PASS: '000000nghia',
+    DB_DATABASE: 'shoppingbook',
+    EMAIL_USER: 'phamphuocnghia21082002@gmail.com',
+    EMAIL_PASS: 'sjklmevaqlwclejo',
+    JWT_SECRET: 'celica12345',
+    JWT_EXPIRES: '3600000', // in milliseconds
+  };
+  module.exports = MyConstants;
